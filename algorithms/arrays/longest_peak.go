609bf291f113dbc723fcb8de0488d9376160d502
baybaraandrey <baybaraandrey@gmail.com>
2021-05-25 22:39:06 +0300

    [ARRAYS] Refactor LongestPeak

--- algorithms/arrays/longest_peak.go
+++ algorithms/arrays/longest_peak.go
@@ -5,26 +5,15 @@ func LongestPeak(x []int) int {
 	if len(x) < 3 {
 		return 0
 	}
-
-	peaks := findPeakIndexes(x)
-	for _, peak := range peaks {
-		currentPeak := getLeftAvailableSteps(x, peak) + getRightAvailableSteps(x, peak) + 1
-		if currentPeak > largestPeak {
-			largestPeak = currentPeak
-		}
-	}
-
-	return largestPeak
-}
-
-func findPeakIndexes(x []int) []int {
-	peaks := []int{}
 	for i := 1; i < len(x)-1; i++ {
 		if x[i-1] < x[i] && x[i] > x[i+1] {
-			peaks = append(peaks, i)
+			currentPeak := getLeftAvailableSteps(x, i) + getRightAvailableSteps(x, i) + 1
+			if currentPeak > largestPeak {
+				largestPeak = currentPeak
+			}
 		}
 	}
-	return peaks
+	return largestPeak
 }
 
 func getLeftAvailableSteps(x []int, peakIndex int) int {
