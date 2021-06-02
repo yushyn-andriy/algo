d887384fac92966c88ad870a97677780497e6394
baybaraandrey <baybaraandrey@gmail.com>
2021-06-03 00:25:30 +0300

    [TREES] Add MinHeightBST

--- algorithms/trees/node_depths.go
+++ algorithms/trees/node_depths.go
@@ -1,5 +1,9 @@
 package trees
 
+import (
+	"sort"
+)
+
 func NodeDepths(root *Tree) int {
 	st := NewTreeStack()
 	visited := make(map[*Tree]bool)
@@ -76,4 +80,44 @@ func NodeDepthsMax(root *Tree) int {
 	}
 
 	return depthsMax
+}
+
+// MinHeightBST
+func MinHeightBST(x []int) *Tree {
+	root := &Tree{Key: x[0]}
+	if len(x) == 1 {
+		return root
+	}
+
+	sort.Ints(x)
+	arr := BuildArr(x)
+
+	for _, key := range arr {
+		Insert(root, &Tree{Key: key})
+	}
+	return root
+}
+
+func BuildArr(x []int) []int {
+	if len(x) == 1 {
+		return []int{x[0]}
+	}
+	if len(x) == 2 {
+		return []int{x[0], x[1]}
+	}
+	if len(x) == 3 {
+		return []int{x[1], x[0], x[2]}
+	}
+
+	getMiddle := func(left, right int) int {
+		return (left + right) / 2
+	}
+
+	mid := getMiddle(0, len(x))
+	arr := []int{x[mid]}
+
+	arr = append(arr, BuildArr(x[0:mid])...)
+	arr = append(arr, BuildArr(x[mid+1:])...)
+
+	return arr
 }
--- algorithms/trees/node_depths_test.go
+++ algorithms/trees/node_depths_test.go
@@ -1,6 +1,7 @@
 package trees_test
 
 import (
+	"reflect"
 	"testing"
 
 	"github.com/baybaraandrey/algo/algorithms/trees"
@@ -74,6 +75,44 @@ func TestNodeDepthsMax(t *testing.T) {
 		nodeDepths := trees.NodeDepthsMax(root)
 		if nodeDepths != testCase.expectedNodeDepthsMax {
 			t.Errorf("Expected depthsMax=%v, got=%v", testCase.expectedNodeDepthsMax, nodeDepths)
+		}
+	}
+}
+
+func TestMinHeightBST(t *testing.T) {
+	tests := []struct {
+		x                  []int
+		expectedBuildedArr []int
+		expectedDepthsMax  int
+	}{
+		{
+			[]int{0, 1, 3, 4, 5, 6, 10},
+			[]int{4, 1, 0, 3, 6, 5, 10},
+			3,
+		},
+		{
+			[]int{2, 3, 5, 6, 8, 10, 12, 13, 14, 17},
+			[]int{10, 5, 2, 3, 6, 8, 14, 12, 13, 17},
+			4,
+		},
+		{
+			[]int{1, 6, 10, 11, 14, 15, 17, 19, 20},
+			[]int{14, 10, 1, 6, 11, 19, 15, 17, 20},
+			4,
+		},
+	}
+
+	for i, test := range tests {
+		root := trees.MinHeightBST(test.x)
+		nodeDepthsMax := trees.NodeDepthsMax(root)
+		if nodeDepthsMax != test.expectedDepthsMax {
+			t.Errorf("test(%d) Expected depthsMax=%v, got=%v", i, test.expectedDepthsMax, nodeDepthsMax)
+			return
+		}
+		buildedArr := trees.BuildArr(test.x)
+		if !reflect.DeepEqual(buildedArr, test.expectedBuildedArr) {
+			t.Errorf("test(%d) Expected buildedArr=%v, got=%v", i, test.expectedBuildedArr, buildedArr)
+			return
 		}
 	}
 }
