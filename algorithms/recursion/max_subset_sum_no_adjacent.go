cfead656783deb282851bc629faec06a24f2d4e7
andre_puankare <baybaraandrey@gmail.com>
2021-06-06 18:25:14 +0300

    Add MaxSubsetSumNoAdjacent

---
+++ algorithms/recursion/max_subset_sum_no_adjacent.go
@@ -0,0 +1,51 @@
+package recursion
+
+import (
+	"fmt"
+	"math"
+)
+
+func MaxSubsetSumNoAdjacent(x []int) int {
+	if len(x) < 1 {
+		return 0
+	}
+	if len(x) == 1 {
+		return x[0]
+	}
+	if len(x) == 2 {
+		return int(math.Max(float64(x[0]), float64(x[1])))
+	}
+
+	return maxSubsetSum(x)
+}
+
+func maxSubsetSum(x []int) int {
+	subsetSumArr := make([]float64, len(x))
+	subsetSumArr[0] = float64(x[0])
+	subsetSumArr[1] = math.Max(float64(x[0]), float64(x[1]))
+	for i := 2; i < len(subsetSumArr); i++ {
+		subsetSumArr[i] = math.Inf(-1)
+	}
+
+	for i := 2; i < len(subsetSumArr); i++ {
+		subsetSumArr[i] = subsetSumsElement(x, subsetSumArr, i)
+	}
+
+	m := math.Inf(-1)
+	for _, val := range subsetSumArr {
+		if val > m {
+			m = val
+		}
+	}
+	return int(m)
+}
+
+func subsetSumsElement(x []int, s []float64, i int) float64 {
+	if s[i] > math.Inf(-1) {
+		return s[i]
+	}
+	return math.Max(
+		subsetSumsElement(x, s, i-1),
+		subsetSumsElement(x, s, i-2)+float64(x[i]),
+	)
+}
---
+++ algorithms/recursion/max_subset_sum_no_adjacent_test.go
@@ -0,0 +1,42 @@
+package recursion_test
+
+import (
+	"testing"
+
+	"github.com/baybaraandrey/algo/algorithms/recursion"
+)
+
+func TestMaxSubsetSumNoAdjacent(t *testing.T) {
+	tests := []struct {
+		x []int // input array
+		m int   // expected max ubset sum no adjacent
+	}{
+		{
+			[]int{7, 10, 12, 7, 9, 14},
+			33,
+		},
+		{
+			[]int{},
+			0,
+		},
+		{
+			[]int{1},
+			1,
+		},
+		{
+			[]int{1, 2},
+			2,
+		},
+		{
+			[]int{9, 2, 3, 4, 5, 13},
+			26,
+		},
+	}
+
+	for i, test := range tests {
+		m := recursion.MaxSubsetSumNoAdjacent(test.x)
+		if m != test.m {
+			t.Errorf("test(%d): Wrong subset sum,  expected %d but got %d", i, test.m, m)
+		}
+	}
+}
