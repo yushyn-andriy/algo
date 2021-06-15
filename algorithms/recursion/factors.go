a41d890c69b314a60c192b3cadd4221380f21a90
baybaraandrey <baybaraandrey@gmail.com>
2021-06-15 11:07:23 +0300

    Refactor

--- algorithms/recursion/factors.go
+++ algorithms/recursion/factors.go
@@ -1,5 +1,10 @@
 package recursion
 
+import (
+	"math"
+	"sort"
+)
+
 func Factors(fn []int) [][][]int {
 	r := [][][]int{}
 	factors(fn, len(fn), [][]int{}, &r)
@@ -42,4 +47,46 @@ func factors(fn []int, fnLen int, currCombination [][]int, r *[][][]int) {
 			fn = append(fn, el)
 		}
 	}
+}
+
+func FlattenFactorCombinations(x [][][]int) [][]int {
+	flatten := [][]int{}
+	for _, factors := range x {
+		r := []int{}
+		for _, combinations := range factors {
+			r = append(r, mul(combinations))
+		}
+		flatten = append(flatten, r)
+	}
+	return flatten
+}
+
+func mul(x []int) int {
+	s := 1
+	for _, v := range x {
+		s *= v
+	}
+	return s
+}
+
+func FindNumbers(x [][]int) []int {
+	y := []int{}
+	var primeNumbers []int = []int{
+		2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 71,
+		73, 79, 83, 90, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151,
+	}
+
+	r := 1
+	for _, combination := range x {
+		sort.Sort(sort.Reverse(sort.IntSlice(combination)))
+		for i, p := range combination {
+			r *= int(math.Pow(float64(primeNumbers[i]), float64(p-1)))
+		}
+		if r > 0 {
+			y = append(y, r)
+		}
+		r = 1
+	}
+
+	return y
 }
--- algorithms/recursion/factors_test.go
+++ algorithms/recursion/factors_test.go
@@ -81,3 +81,123 @@ func TestFactors(t *testing.T) {
 		}
 	}
 }
+
+func TestFlattenFactorCombinations(t *testing.T) {
+	tests := []struct {
+		cs [][][]int
+		cm [][]int
+	}{
+		{
+			[][][]int{
+				{
+					{3},
+					{2},
+					{1},
+				},
+				{
+					{3},
+					{2, 1},
+				},
+				{
+					{3, 2},
+					{1},
+				},
+				{
+					{3, 2, 1},
+				},
+			},
+			[][]int{
+				{3, 2, 1},
+				{3, 2},
+				{6, 1},
+				{6},
+			},
+		},
+
+		{
+			[][][]int{
+				{
+					{2},
+					{1},
+				},
+				{
+					{2, 1},
+				},
+			},
+			[][]int{
+				{2, 1},
+				{2},
+			},
+		},
+		{
+			[][][]int{
+				{
+					{17},
+					{2},
+					{2},
+				},
+				{
+					{17},
+					{2, 2},
+				},
+				{
+					{17, 2},
+					{2},
+				},
+				{
+					{17, 2, 2},
+				},
+			},
+			[][]int{
+				{17, 2, 2},
+				{17, 4},
+				{34, 2},
+				{68},
+			},
+		},
+	}
+
+	for i, test := range tests {
+		cm := recursion.FlattenFactorCombinations(test.cs)
+		if len(cm) != len(test.cm) {
+			t.Errorf("test(%d) Expected %v got %v", i, cm, test.cm)
+			continue
+		}
+		for j, l := range cm {
+			if !reflect.DeepEqual(test.cm[j], l) {
+				t.Errorf("test(%d) subtest(%d) expected %v got %v", i, j, test.cm[j], l)
+			}
+		}
+	}
+}
+
+func TestFindNumbers(t *testing.T) {
+	tests := []struct {
+		cm [][]int
+		ns []int
+	}{
+		{
+			[][]int{
+				{3, 2},
+				{6},
+			},
+			[]int{12, 32},
+		},
+		{
+			[][]int{
+				{11, 3, 2},
+				{33, 2},
+				{11, 6},
+				{65},
+			},
+			[]int{46080, 12884901888, 248832},
+		},
+	}
+
+	for i, test := range tests {
+		ns := recursion.FindNumbers(test.cm)
+		if !reflect.DeepEqual(ns, test.ns) {
+			t.Errorf("test(%d) Expected %v got %v", i, test.ns, ns)
+		}
+	}
+}
