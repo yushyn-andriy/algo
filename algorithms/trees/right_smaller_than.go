32024e755e747ab4bd102a721b18aa88f80aec7f
baybaraandrey <baybaraandrey@gmail.com>
2021-07-17 20:01:54 +0300

    [TREES] RightSmallerThan

--- algorithms/trees/right_smaller_than.go
+++ algorithms/trees/right_smaller_than.go
@@ -13,3 +13,71 @@ func RightSmallerThan1(array []int) []int {
 
 	return smaller
 }
+
+func RightSmallerThan2(array []int) []int {
+	if len(array) == 0 {
+		return []int{}
+	}
+
+	smaller := make([]int, len(array))
+	bst := &BSTree{Value: array[len(array)-1], IndexInArray: len(array) - 1}
+	for i := len(array) - 2; i >= 0; i-- {
+		bst.Insert(array[i], 0, i)
+	}
+
+	return bst.SmallerCounterInOrderTraverse(smaller)
+}
+
+type BSTree struct {
+	Value          int
+	Left           *BSTree
+	Right          *BSTree
+	LeftCounter    int
+	SmallerCounter int
+	IndexInArray   int
+}
+
+func (tree *BSTree) Insert(value int, smallerCount, indexInArray int) {
+	if tree.Value > value {
+		tree.LeftCounter += 1
+		if tree.Left == nil {
+			tree.Left = &BSTree{
+				Value:          value,
+				SmallerCounter: smallerCount,
+				IndexInArray:   indexInArray,
+			}
+		} else {
+			tree.Left.Insert(value, smallerCount, indexInArray)
+		}
+	} else {
+		additionalNodeSmallerCount := 0
+		if tree.Value < value {
+			additionalNodeSmallerCount = 1
+		}
+		if tree.Right == nil {
+
+			tree.Right = &BSTree{
+				Value:          value,
+				SmallerCounter: tree.LeftCounter + smallerCount + additionalNodeSmallerCount,
+				IndexInArray:   indexInArray,
+			}
+		} else {
+			tree.Right.Insert(value, smallerCount+tree.LeftCounter+additionalNodeSmallerCount, indexInArray)
+		}
+	}
+}
+
+func (tree *BSTree) SmallerCounterInOrderTraverse(array []int) []int {
+	smallerCounterTraverse(tree, &array)
+	return array
+}
+
+func smallerCounterTraverse(tree *BSTree, array *[]int) {
+	if tree == nil {
+		return
+	}
+
+	(*array)[tree.IndexInArray] = tree.SmallerCounter
+	smallerCounterTraverse(tree.Left, array)
+	smallerCounterTraverse(tree.Right, array)
+}
--- algorithms/trees/right_smaller_than_test.go
+++ algorithms/trees/right_smaller_than_test.go
@@ -25,6 +25,41 @@ func TestRightSmallerThan(t *testing.T) {
 	}
 }
 
+func TestRightSmallerThanSolution2(t *testing.T) {
+	tests := []struct {
+		in  []int
+		out []int
+	}{
+		{
+			[]int{8, 5, 11, -1, 3, 4, 2},
+			[]int{5, 4, 4, 0, 1, 1, 0},
+		},
+		{
+			[]int{0, 1, 1, 2, 3, 5, 8, 13},
+			[]int{0, 0, 0, 0, 0, 0, 0, 0},
+		},
+		{
+			[]int{},
+			[]int{},
+		},
+		{
+			[]int{1, 0},
+			[]int{1, 0},
+		},
+		{
+			[]int{0, 1},
+			[]int{0, 0},
+		},
+	}
+
+	for i, test := range tests {
+		out := trees.RightSmallerThan2(test.in)
+		if !IsEqual(out, test.out) {
+			t.Errorf("test(%d) expected %v got %v", i, test.out, out)
+		}
+	}
+}
+
 func IsEqual(a, b []int) bool {
 	if len(a) != len(b) {
 		return false
