9ac35ad3c84e7d9061263a1068680e797f1ceaf9
baybaraandrey <baybaraandrey@gmail.com>
2021-07-21 21:01:32 +0300

    [BINARY-TREES] FindNodesDistanceK

---
+++ algorithms/binarytrees/find_nodes_distance_k_test.go
@@ -0,0 +1,58 @@
+package binarytrees
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestFindNodesDistanceK(t *testing.T) {
+	tests := []struct {
+		in        *BinaryTree
+		target, k int
+		array     []int
+	}{
+		{
+			&BinaryTree{
+				Value: 1,
+				Left: &BinaryTree{
+					Value: 2,
+					Left: &BinaryTree{
+						Value: 4,
+					},
+					Right: &BinaryTree{
+						Value: 5,
+					},
+				},
+				Right: &BinaryTree{
+					Value: 3,
+					Left: &BinaryTree{
+						Value: 6,
+					},
+					Right: &BinaryTree{
+						Value: 7,
+					},
+				},
+			},
+			4, 2,
+			[]int{5, 1},
+		},
+		{
+			&BinaryTree{
+				Value: -2,
+				Left: &BinaryTree{
+					Value: -1,
+				},
+			},
+			-1, 1,
+			[]int{-2},
+		},
+	}
+
+	for i, test := range tests {
+		out := FindNodesDistanceK(test.in, test.target, test.k)
+		if !reflect.DeepEqual(out, test.array) {
+			t.Errorf("test(%d) expected %v got %v", i, test.array, out)
+		}
+	}
+
+}
