d14ffd2488c57628d7c5fae6a34b0bdd0223b1f6
baybaraandrey <baybaraandrey@gmail.com>
2021-07-28 21:58:32 +0300

    [SORTING] ThreeNumberSort

---
+++ algorithms/sorting/three_number_sort.go
@@ -0,0 +1,25 @@
+package sorting
+
+
+func ThreeNumberSort(array []int, order []int) []int {
+	firstValue, secondValue := order[0], order[1]
+
+	firstIdx, secondIdx, thirdIdx := 0, 0, len(array) - 1
+
+	for secondIdx <= thirdIdx {
+		value := array[secondIdx]
+
+		if value == firstValue {
+			array[firstIdx], array[secondIdx] = array[secondIdx], array[firstIdx]
+			firstIdx += 1
+			secondIdx += 1
+		} else if value == secondValue {
+			secondIdx += 1
+		} else {
+			array[secondIdx], array[thirdIdx] = array[thirdIdx], array[secondIdx]
+			thirdIdx -= 1
+		}
+	}
+
+	return array
+}
