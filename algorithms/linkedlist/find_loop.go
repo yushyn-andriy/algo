b0ad4158c2dd9552e77abcc39745720c07df47d9
baybaraandrey <baybaraandrey@gmail.com>
2021-07-31 20:48:05 +0300

    [LINKEDLIST] FindLoopAlgoSolution

--- algorithms/linkedlist/find_loop.go
+++ algorithms/linkedlist/find_loop.go
@@ -16,3 +16,24 @@ func FindLoop(head *LinkedListLoop) *LinkedListLoop {
 	}
 	return nil
 }
+
+// o(n) time | o(1) space
+// T = 2d + p
+// F -> x -> d + p
+// S -> 2x -> 2d + 2p
+// R = t  - p - d
+// r = 2d + p - p - d = d
+func FindLoopAlgoSolution(head *LinkedListLoop) *LinkedListLoop {
+	first := head.Next
+	second := first.Next
+	for first != second {
+		first = first.Next
+		second = second.Next
+	}
+	first = head
+	for first != second {
+		first = first.Next
+		second = second.Next
+	}
+	return first
+}
