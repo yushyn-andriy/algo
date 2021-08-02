de77f848771386d8c2def426784190b3b4fe1852
baybaraandrey <baybaraandrey@gmail.com>
2021-08-02 08:55:21 +0300

    [LINKEDLIST] RemoveKthNodeFromEndAlgoSolution

--- algorithms/linkedlist/remove_kth_node_from_end.go
+++ algorithms/linkedlist/remove_kth_node_from_end.go
@@ -22,3 +22,22 @@ func RemoveKthNodeFromEnd(head *LinkedList, k int) {
 		position--
 	}
 }
+
+func RemoveKthNodeFromEndAlgoSolution(head *LinkedList, k int) {
+	counter, first, second := 1, head, head
+	for counter <= k {
+		second = second.Next
+		counter += 1
+	}
+	if second == nil {
+		head.Value = head.Next.Value
+		head.Next = head.Next.Next
+		return
+	}
+
+	for second.Next != nil {
+		second = second.Next
+		first = first.Next
+	}
+	first.Next = first.Next.Next
+}
---
+++ algorithms/linkedlist/shift_linked_list.go
@@ -0,0 +1,4 @@
+package linkedlist
+
+
+func 
