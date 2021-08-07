8d26b121c625b58b01c4f5e8cfb3da3a0dc2fa06
baybaraandrey <baybaraandrey@gmail.com>
2021-08-07 21:34:07 +0300

    [STRINGS] GroupAnagrams

---
+++ algorithms/strings/group_anagrams.go
@@ -0,0 +1,27 @@
+package strings
+
+import (
+	"sort"
+)
+
+func GroupAnagrams(words []string) [][]string {
+	anagrams := map[string][]string{}
+	for _, word := range words {
+		sortedWord := sortWord(word)
+		anagrams[sortedWord] = append(anagrams[sortedWord], word)
+	}
+
+	result := [][]string{}
+	for _, group := range anagrams {
+		result = append(result, group)
+	}
+	return result
+}
+
+func sortWord(word string) string {
+	wordBytes := []byte(word)
+	sort.Slice(wordBytes, func(i, j int) bool {
+		return wordBytes[i] < wordBytes[j]
+	})
+	return string(wordBytes)
+}
