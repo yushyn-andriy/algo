80cf0d3f01aec37714b67248d2955c279f9e21d9
baybaraandrey <baybaraandrey@gmail.com>
2021-08-12 21:08:40 +0300

    [STACK] MinMaxStack

--- algorithms/stack/stack.go
+++ algorithms/stack/stack.go
@@ -22,3 +22,70 @@ func (s *Stack) Pop() (int, bool) {
 func New() *Stack {
 	return &Stack{}
 }
+
+type MinMaxStack struct {
+	data                 map[int]int
+	currentIdx, min, max int
+	minMaxStack          []entry
+}
+
+type entry struct {
+	min, max int
+}
+
+func NewMinMaxStack() *MinMaxStack {
+	return &MinMaxStack{
+		data:        map[int]int{},
+		currentIdx:  0,
+		minMaxStack: []entry{},
+	}
+}
+
+func (stack *MinMaxStack) Peek() int {
+	v, _ := stack.data[stack.currentIdx-1]
+	return v
+}
+
+func (stack *MinMaxStack) Pop() int {
+	stack.minMaxStack = stack.minMaxStack[:len(stack.minMaxStack)-1]
+	stack.currentIdx--
+	v, found := stack.data[stack.currentIdx]
+	if found {
+		delete(stack.data, stack.currentIdx)
+	}
+	return v
+}
+
+func (stack *MinMaxStack) Push(number int) {
+	newMinMax := entry{min: number, max: number}
+	if len(stack.minMaxStack) > 0 {
+		lastMinMax := stack.minMaxStack[len(stack.minMaxStack)-1]
+		newMinMax.min = min(lastMinMax.min, number)
+		newMinMax.max = max(lastMinMax.max, number)
+	}
+	stack.minMaxStack = append(stack.minMaxStack, newMinMax)
+	stack.data[stack.currentIdx] = number
+	stack.currentIdx += 1
+}
+
+func (stack *MinMaxStack) GetMin() int {
+	return stack.minMaxStack[len(stack.minMaxStack)-1].min
+}
+
+func (stack *MinMaxStack) GetMax() int {
+	return stack.minMaxStack[len(stack.minMaxStack)-1].max
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
