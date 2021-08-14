3e869e7e017854f2e08efa75ce4613ef29460f54
baybaraandrey <baybaraandrey@gmail.com>
2021-08-14 23:46:39 +0300

    [GREEDY] MinimumWaitingTime

---
+++ algorithms/greedy/minimum_waiting_time.go
@@ -0,0 +1,17 @@
+package greedy
+
+import (
+	"sort"
+)
+
+func MinimumWaitingTime(queries []int) int {
+	sort.Ints(queries)
+
+	totalWaitingTime := 0
+	for idx, duration := range queries {
+		queriesLeft := len(queries) - (idx + 1)
+		totalWaitingTime += duration * queriesLeft
+	}
+
+	return totalWaitingTime
+}
