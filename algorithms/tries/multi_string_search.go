f2410c05b1f074ea73fd98d1636c6accbc51ad41
baybaraandrey <baybaraandrey@gmail.com>
2022-01-19 22:18:53 +0200

    [TRIES] MultiStringSearch1

--- algorithms/tries/multi_string_search.go
+++ algorithms/tries/multi_string_search.go
@@ -1,6 +1,36 @@
 package tries
 
+func check1(bigString, smallString string, start int) bool {
+	bigStringStartIdx := start
+	bigStringEndIdx := bigStringStartIdx + len(smallString) - 1
+	smallStringStartIdx := 0
+	smallStringEndIdx := len(smallString) - 1
+
+	if bigStringEndIdx > len(bigString)-1 {
+		return false
+	}
+
+	for bigStringStartIdx < bigStringEndIdx {
+		if bigString[bigStringStartIdx] != smallString[smallStringStartIdx] ||
+			bigString[bigStringEndIdx] != smallString[smallStringEndIdx] {
+			return false
+		}
+		bigStringStartIdx++
+		bigStringEndIdx--
+		smallStringStartIdx++
+		smallStringEndIdx--
+	}
+
+	return true
+}
+
 func isInBigString1(bigString, smallString string) bool {
+	for startIdx := 0; startIdx < len(bigString)-(len(smallString)-1); startIdx++ {
+		if in := check1(bigString, smallString, startIdx); in {
+			return true
+		}
+	}
+
 	return false
 }
 
